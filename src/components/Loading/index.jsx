61b86c82c4c33102e2eef31a0df7750b43f20ff0
EzozaMukhamedova <ezoza.muxamedova@gmail.com>
2025-02-20 12:38:56 +0900

    error loading

--- src/components/Loading/index.jsx
+++ src/components/Loading/index.jsx
@@ -3,12 +3,12 @@ import React from "react";
 export default function Loading() {
   return (
     <div>
-      {/* <div className="flex items-center justify-center w-56 h-56 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
-        <div className="px-3 py-1 text-xs font-medium leading-none text-center text-blue-800 bg-blue-200 rounded-full animate-pulse dark:bg-blue-900 dark:text-blue-200">
+      <div className="flex items-center mx-auto w-[500px] h-[300px] justify-center border-gray-200 rounded-lg   dark:border-gray-700">
+        <div className="px-4 py-2 text-[16px] font-medium leading-none text-center text-blue-800 bg-blue-200 rounded-full animate-pulse dark:bg-blue-500 dark:text-blue-200">
           loading...
         </div>
-      </div> */}
-      <div className="w-[238px] h-[425px] flex justify-center items-center">
+      </div>
+      {/* <div className="w-[238px] h-[425px] flex justify-center items-center">
         <div className="loading_container">
           <div className="line"></div>
           <div className="line"></div>
@@ -17,7 +17,7 @@ export default function Loading() {
           <div className="line"></div>
           <div className="line"></div>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -2,16 +2,28 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import Navbar from "../components/Navbar";
+import Loading from "../components/Loading";
 
 const Product = () => {
   const [product, setProduct] = useState({});
   const { id } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     async function fetchProduct() {
-      const res = await axios.get(`http://localhost:5000/products/${id}`);
-      console.log(res.data.data);
-      setProduct(res.data.data);
+      try {
+        setLoading(true);
+        const res = await axios.get(`http://localhost:15000/products/${id}`);
+        console.log(res.data.data);
+        setProduct(res.data.data);
+      } catch (err) {
+        console.log(err.message);
+
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
     }
     fetchProduct();
   }, []);
@@ -24,56 +36,77 @@ const Product = () => {
     // </div>
     <>
       <Navbar />
-      <div className="flex justify-center  py-[100px] bg-gray-100">
-        <div className="flex max-w-[1000px] h-[500px] overflow-hidden bg-white border border-blue-300 shadow-lg rounded-lg">
-          <div className="w-1/2">
-            <img
-              className="object-cover w-full h-full rounded-l-lg"
-              src={product.image}
-              alt={product.name}
+      {loading && <Loading />}
+      {error && (
+        <div
+          className="flex items-center justify-center w-[500px] mx-auto mt-[100px] p-4 mb-4 text-sm text-red-700 bg-red-100 border border-red-400 rounded-lg"
+          role="alert"
+        >
+          <svg className="w-5 h-5 mr-3" fill="currentColor" viewBox="0 0 20 20">
+            <path
+              fill-rule="evenodd"
+              d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v5a1 1 0 102 0V6a1 1 0 00-1-1z"
+              clip-rule="evenodd"
             />
-          </div>
-          <div className="w-1/2 p-4">
-            <div className="flex justify-between">
-              <span className="text-[20px] font-semibold text-blue-500">
-                SKIDKA -15%
-              </span>
-              <div className="flex items-center text-gray-500">
-                <span className="ml-1 text-[18px] text-blue-500">
-                  ⭐️ {product.rating}{" "}
+          </svg>
+          <p>Error: {error}</p>
+        </div>
+      )}
+      {/* <h1 className="bg-red-500">Error: {error}</h1>} */}
+      {!loading && !error && (
+        <div className="flex justify-center  py-[100px] bg-gray-100">
+          <div className="flex max-w-[1000px] h-[500px] overflow-hidden bg-white border border-blue-300 shadow-lg rounded-lg">
+            <div className="w-1/2">
+              <img
+                className="object-cover w-full h-full rounded-l-lg"
+                src={product.image}
+                alt={product.name}
+              />
+            </div>
+            <div className="w-1/2 p-4">
+              <div className="flex justify-between">
+                <span className="text-[20px] font-semibold text-blue-500">
+                  SKIDKA -15%
                 </span>
+                <div className="flex items-center text-gray-500">
+                  <span className="ml-1 text-[18px] text-blue-500">
+                    ⭐️ {product.rating}{" "}
+                  </span>
+                </div>
               </div>
-            </div>
-            <h3 className="mt-2 mb-2 text-[25px] font-bold">{product.name}</h3>
-            <div className="flex items-center justify-between">
-              <span className="text-[25px] font-bold text-red-500">
-                {product.price} so'm
-              </span>
-              <button className="px-3 py-2 text-[16px] text-white  bg-blue-500 rounded hover:bg-blue-600">
-                Tarqatish
-              </button>
-            </div>
-            <p className="mt-4 text-[16px]  text-gray-500">
-              Brend: ..... {product.category}
-            </p>
-            <p className="text-[16px]  text-gray-500">
-              Model: ..... {product.description}
-            </p>
-            <p className="text-[16px]  text-gray-500">
-              Qoldiq: .....
-              {product.stock} ta
-            </p>
-            <div className="flex mt-4 space-x-2">
-              <button className="px-5 py-3 text-[18px] text-white bg-green-500 rounded cursor-pointer hover:bg-green-600">
-                Savatchaga qo'shing
-              </button>
-              <button className="px-5 py-3 text-[18px] text-blue-500 border border-blue-500 rounded hover:bg-blue-600 hover:text-white cursor-pointer">
-                Bir marta bosib sotib oling
-              </button>
+              <h3 className="mt-2 mb-2 text-[25px] font-bold">
+                {product.name}
+              </h3>
+              <div className="flex items-center justify-between">
+                <span className="text-[25px] font-bold text-red-500">
+                  {product.price} so'm
+                </span>
+                <button className="px-3 py-2 text-[16px] text-white  bg-blue-500 rounded hover:bg-blue-600">
+                  Tarqatish
+                </button>
+              </div>
+              <p className="mt-4 text-[16px]  text-gray-500">
+                Brend: ..... {product.category}
+              </p>
+              <p className="text-[16px]  text-gray-500">
+                Model: ..... {product.description}
+              </p>
+              <p className="text-[16px]  text-gray-500">
+                Qoldiq: .....
+                {product.stock} ta
+              </p>
+              <div className="flex mt-4 space-x-2">
+                <button className="px-5 py-3 text-[18px] text-white bg-green-500 rounded cursor-pointer hover:bg-green-600">
+                  Savatchaga qo'shing
+                </button>
+                <button className="px-5 py-3 text-[18px] text-blue-500 border border-blue-500 rounded hover:bg-blue-600 hover:text-white cursor-pointer">
+                  Bir marta bosib sotib oling
+                </button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
     </>
   );
 };
